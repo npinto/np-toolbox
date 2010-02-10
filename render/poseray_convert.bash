b0a7c94a3cab4e515386bc54cac98dad2112b761
Nicolas Pinto <pinto@mit.edu>
2010-02-10 18:37:24 -0500

    Adapt poseray convert script to allow multiple instances at the same time

--- render/poseray_convert.bash
+++ render/poseray_convert.bash
@@ -11,6 +11,11 @@ fi;
 
 MYPATH=$(dirname "$0")
 
+TMP_SUFFIX=$(mktemp -d)
+TMP_POSERAY_PATH="${POSERAY_PATH}${TMP_SUFFIX}"
+mkdir -p "$TMP_POSERAY_PATH"
+cp -vf "$POSERAY_PATH"/PoseRay.exe "$TMP_POSERAY_PATH"/PoseRay.exe
+
 INPUT="$1"
 OUTPUT=\
 $(python -c "import sys, os;
@@ -22,16 +27,17 @@ INPUT_NOEXT=\
 $(python -c "import sys, os;
 print os.path.split(os.path.splitext(sys.argv[1])[0])[-1]" $INPUT)
 INPUT_NOEXT=$(echo $INPUT_NOEXT | sed 's/\./_/') # Replace . with _
-
-# backup previous poseray settings
-cp -vf "$POSERAY_PATH"/poseraysettings.ini{,.bak}
+INPUT_NOEXT=$(echo $INPUT_NOEXT | sed 's/-/_/') # Replace - with _
 
 # replace with our settings
-sed "s/export/export\\/$INPUT_FNAME/" poseraysettings.ini.wine > "$POSERAY_PATH"/poseraysettings.ini
-#cp -vf "$MYPATH"/poseraysettings.ini.wine "$POSERAY_PATH"/poseraysettings.ini
-mkdir -p "$POSERAY_PATH/export/$INPUT_FNAME"
+#sed "s/export/export\/$INPUT_FNAME/" poseraysettings.ini.wine > "$TMP_POSERAY_PATH"/poseraysettings.ini
+sed "s|export|$TMP_SUFFIX/export/$INPUT_FNAME|g" poseraysettings.ini.wine > "$TMP_POSERAY_PATH"/poseraysettings.ini
+grep savetodir "$TMP_POSERAY_PATH"/poseraysettings.ini
+EXPORT_DIR="$TMP_POSERAY_PATH/export/$INPUT_FNAME"
+mkdir -p "$EXPORT_DIR"
+#exit 99
 
-echo Wine temporary $(grep savetodir "$POSERAY_PATH"/poseraysettings.ini)
+echo Wine temporary $(grep savetodir "$TMP_POSERAY_PATH"/poseraysettings.ini)
 echo "OUTPUT=$OUTPUT"
 #exit 99
 
@@ -40,20 +46,23 @@ echo Processing ...
 set -x 
 
 cd $INPUT_DIR && 
-wine "$POSERAY_PATH"/PoseRay.exe -load $INPUT_FNAME -convert 1 -close && 
+wine "$TMP_POSERAY_PATH"/PoseRay.exe -load $INPUT_FNAME -convert 1 -close < /dev/null && 
 mkdir -p $OUTPUT &&
-mv -f "$POSERAY_PATH"/export/$INPUT_FNAME/* $OUTPUT/ &> /dev/null && 
-cd $OUTPUT && 
-sed 's/^background .*/background { color rgb<.5,.5,.5>  }/' *_POV_scene.pov > *_POV_scene.pov.new &&
-mv -f *_POV_scene.pov{.new,} &&
-$POVRAY_CMD +L./ +I./${INPUT_NOEXT}_POV_scene.pov +O./${INPUT_NOEXT}_POV_scene.png
+mv -f "$EXPORT_DIR"/* $OUTPUT/ &> /dev/null && 
+cd $OUTPUT && ls -alF &&
+for pov in *_POV_scene.pov;
+do sed 's/^background .*/background { color rgb<.5,.5,.5>  }/' $pov > $pov.new &&
+mv -f $pov{.new,} &&
+#$POVRAY_CMD +L./ +I./${INPUT_NOEXT}_POV_scene.pov +O./${INPUT_NOEXT}_POV_scene.png 
+$POVRAY_CMD +L./ +I./$pov +O./$pov.png;
+done;
 
-# revert settings changes
-cp -vf "$POSERAY_PATH"/poseraysettings.ini{.bak,}
+rm -rvf "$TMP_POSERAY_PATH"
+
+set -
 
 echo Done!
 
-set -
 
 
 
