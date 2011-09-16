d516f784950a4ead9fe0bab9e225d0c5c38dc662
Nicolas Pinto <pinto@mit.edu>
2011-09-16 01:29:57 -0400

    Add docstring convention template

---
+++ snippets/docstrings_pep257_scipy.py
@@ -0,0 +1,48 @@
+# from @ogrisel
+# https://github.com/scikit-learn/scikit-learn/pull/310
+
+"""Single line description of the method (watch the 80 chars limit)
+
+Additional description of the method giving motivations and details
+that could not fit in the single line description of the method. Note
+the emtpty line that separate the first line and the first paragraph.
+Also note the indenting of the first line (no leading space) and the
+paragraphs.
+
+This description can be multple paragraphs long if appropriates. The
+arguments of the method / function are not described in details here
+though but in the parameters section that follows.
+
+To format a paragraph on vim, you can use the key sequence: `gqap`.
+
+Parameters
+----------
+
+param_1 : type, shape, [optional, default value]
+    Description of param_1 and some rule of thumb or short theoretical
+    consideration on how to pickup a 'good' value.
+
+param_2 : type, shape, [optional, default value]
+    Description of param_1 and some rule of thumb or short theoretical
+    consideration on how to pickup a 'good' value.
+
+Return
+------
+
+return_1 : type, shape
+    Description of the returned value. If there is more than one
+    returned value add new entries to this section.
+
+References
+----------
+
+Bibliographical information of the main papers with author names, year,
+title and link to a PDF if available.
+
+See also
+--------
+
+List other scikit-learn functions or classes that are related
+(alternative implementation that solve the same task for instance).
+"""
+
