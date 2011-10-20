a5b898483d984cba5d4318362a4c643d9db61a25
Nicolas Pinto <pinto@mit.edu>
2011-10-20 15:15:28 -0500

    Update groff work around

--- sysadmin/gentoo_prefix/install_gentoo_prefix.sh
+++ sysadmin/gentoo_prefix/install_gentoo_prefix.sh
@@ -140,7 +140,7 @@ echo 'sys-devel/gcc vanilla' >> $EPREFIX/etc/portage/package.use/gcc
 emerge -u gcc
 #gcc-config 2
 #source $EPREFIX/etc/profile
-emerge -u groff
+MAKEOPTS=-j1 emerge -u groff
 
 emerge -u system
 
