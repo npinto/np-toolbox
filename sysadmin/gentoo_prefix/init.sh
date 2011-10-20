75520b169033de6790a3a98140de65360367493e
Nicolas Pinto <pinto@mit.edu>
2011-10-20 04:32:41 -0500

    Update install mongo install script

---
+++ sysadmin/gentoo_prefix/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
+trap 'echo "exit $? due to $previous_command"' EXIT
+
+# Update environment with prefix variables (EPREFIX, etc.)
+source $(dirname $0)/update_env.sh
--- sysadmin/gentoo_prefix/install_mongodb.sh
+++ sysadmin/gentoo_prefix/install_mongodb.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-source $(dirname $0)/update_env.sh
+source $(dirname $0)/init.sh
 
-#mkdir -p $EPREFIX/usr/local/portage
 (cd $EPREFIX/usr/local/portage && $EPREFIX/usr/portage/scripts/ecopy dev-db/mongodb)
 
-# USE
 echo "dev-db/mongodb v8" >> $EPREFIX/etc/portage/package.use/mongodb
-
-# KEYWORDS
 echo "dev-lang/v8 **" >> $EPREFIX/etc/portage/package.keywords/mongodb
 echo "dev-db/mongodb **" >> $EPREFIX/etc/portage/package.keywords/mongodb
 
-#emerge -v v8
-#emerge -av mongodb
-
-LDFLAGS="-L /usr/lib -L $EPREFIX/usr/lib" CXXLFAGS="-I /usr/include -I $EPREFIX/usr/include" CXX=g++ emerge -v mongodb
-
-#(cd $EPREFIX/usr/local/portage && $EPREFIX/usr/portage/scripts/ecopy dev-lang/spidermonkey)
-#echo 'dev-lang/spidermonkey **' >> $EPREFIX/etc/portage/package.use/mongodb
-
-
+LDFLAGS="-L /usr/lib -L $EPREFIX/usr/lib" CXXFLAGS="-I /usr/include -I $EPREFIX/usr/include" CXX=g++ emerge -v mongodb
 
